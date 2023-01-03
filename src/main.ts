9c45fb1bd1549868abbaa4b13b43b1692f717b29
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-01-04 00:53:09 +0700

    docs: swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Config
+  const config = new DocumentBuilder()
+    .setTitle('Warden NFT Platform')
+    .setDescription('API documentation for the Warden NFT Platform')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { CreateCustomerUserDTO, CreateEventOrganizerUserDTO, LoginDTO } from './dto/user.dto';
+import { ApiConflictResponse, ApiCreatedResponse, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { CreateCustomerUserDTO, CreateEventOrganizerUserDTO, LoginDTO, SuccessfulRegisterDTO } from './dto/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -9,18 +10,24 @@ export class UserController {
 
   @Post('registerCustomer')
   @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse({ type: SuccessfulRegisterDTO })
+  @ApiConflictResponse({ description: 'Email or Phone number already in use' })
   async registerCustomer(@Body() dto: CreateCustomerUserDTO) {
     return this.userService.registerCustomerUser(dto);
   }
 
   @Post('registerEventOrganizer')
   @HttpCode(HttpStatus.CREATED)
+  @ApiConflictResponse({ description: 'Email or Phone number already in use' })
+  @ApiCreatedResponse({ type: SuccessfulRegisterDTO })
   async registerEventOrganizer(@Body() dto: CreateEventOrganizerUserDTO) {
     return this.userService.registerEventOrganizerUser(dto);
   }
 
   @Post('login')
-  @HttpCode(HttpStatus.CREATED)
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: SuccessfulRegisterDTO })
+  @ApiUnauthorizedResponse({ description: 'Incorrect username or password' })
   async login(@Body() dto: LoginDTO) {
     return this.userService.login(dto.phoneNumber, dto.password);
   }
