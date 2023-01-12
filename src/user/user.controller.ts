4e308845ec781f670108192f2d08bddd125d8e8b
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-01-12 17:14:32 +0700

    feat: user verification status update functionality

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
   ApiConflictResponse,
   ApiCreatedResponse,
+  ApiNotFoundResponse,
   ApiOkResponse,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
-import { CreateCustomerUserDTO, CreateEventOrganizerUserDTO, LoginDTO, SuccessfulRegisterDTO } from './dto/user.dto';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import {
+  CreateCustomerUserDTO,
+  CreateEventOrganizerUserDTO,
+  LoginDTO,
+  SuccessfulUserModificationDTO,
+  SuccessfulVerificationDTO,
+  UpdateVerificationStatusDTO,
+} from './dto/user.dto';
 import { UserService } from './user.service';
 
 @ApiTags('Users')
@@ -17,7 +26,7 @@ export class UserController {
 
   @Post('registerCustomer')
   @HttpCode(HttpStatus.CREATED)
-  @ApiCreatedResponse({ type: SuccessfulRegisterDTO })
+  @ApiCreatedResponse({ type: SuccessfulUserModificationDTO })
   @ApiConflictResponse({ description: 'Email or Phone number already in use' })
   async registerCustomer(@Body() dto: CreateCustomerUserDTO) {
     return this.userService.registerCustomerUser(dto);
@@ -26,16 +35,24 @@ export class UserController {
   @Post('registerEventOrganizer')
   @HttpCode(HttpStatus.CREATED)
   @ApiConflictResponse({ description: 'Email or Phone number already in use' })
-  @ApiCreatedResponse({ type: SuccessfulRegisterDTO })
+  @ApiCreatedResponse({ type: SuccessfulUserModificationDTO })
   async registerEventOrganizer(@Body() dto: CreateEventOrganizerUserDTO) {
     return this.userService.registerEventOrganizerUser(dto);
   }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  @ApiOkResponse({ type: SuccessfulRegisterDTO })
+  @ApiOkResponse({ type: SuccessfulUserModificationDTO })
   @ApiUnauthorizedResponse({ description: 'Incorrect username or password' })
   async login(@Body() dto: LoginDTO) {
     return this.userService.login(dto.phoneNumber, dto.password);
+  }
+
+  @Put('setVerificationStatus')
+  @ApiOkResponse({ type: SuccessfulVerificationDTO })
+  @ApiNotFoundResponse({ description: `User id not found` })
+  @UseGuards(JwtAuthGuard)
+  async updateVerificationStatus(@Req() req, @Body() dto: UpdateVerificationStatusDTO) {
+    return this.userService.setUserPhoneVerificationStatus(req.user.uid, dto.verificationStatus);
   }
 }
