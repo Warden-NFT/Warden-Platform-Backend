bb2dc95c5c2990f374947e7b19553a6bef5c775e
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-02-05 21:06:19 +0700

    feat: add ABI typing

--- src/smart-contract/interface/smart-contract.interface.ts
+++ src/smart-contract/interface/smart-contract.interface.ts
@@ -1,8 +1,20 @@
 import * as mongoose from 'mongoose';
 
+export interface InputData {
+  indexed: boolean;
+  internalType: string;
+  name: string;
+  type: string;
+}
+
+export interface ABIItem {
+  anonymous: boolean;
+  inputs: InputData[];
+}
+
 export interface SmartContractABI extends mongoose.Document {
   date: Date;
-  abi: any;
+  abi: ABIItem[];
 }
 
 export interface SmartContractBytecode extends mongoose.Document {
