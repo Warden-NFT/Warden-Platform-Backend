9fcc547884c2fcf2097f8302095fb08e4e505301
ChuChuwiiii <chuwong35122@gmail.com>
2023-02-07 23:10:35 +0700

    feat: remove /saveTicketSet endpoint & improve metadata

--- src/media/Interfaces/MediaUpload.ts
+++ src/media/Interfaces/MediaUpload.ts
@@ -3,14 +3,21 @@ export interface MediaUploadPayload {
   folder: string;
 }
 
-export interface MultipleMediaUploadPayload {
-  folder: string;
-}
-export interface SaveTicketSetDTO {
-  folder: string;
-  ticketMetadata: string;
+export interface FileData {
+  path: string;
+  contentType: string;
+  media: Buffer;
+  metadata: StoredFileMetadata[];
 }
 
+export interface StoredFileMetadata {
+  [key: string]: string;
+}
+
+export interface MultipleMediaUploadPayload {
+  folder: string;
+  metadata: string;
+}
 export interface DeleteMediaDTO {
   path: string;
 }
