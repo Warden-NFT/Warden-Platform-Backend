5dddf9e47e2bff3152caf32ce00149faecde78b6
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-02-10 14:04:57 +0700

    doc: add documentation for media controller, now admin only

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -7,24 +7,34 @@ import {
   Res,
   ServiceUnavailableException,
   UploadedFiles,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
+import { AdminGuard } from 'src/auth/jwt.guard';
 import { StorageService } from 'src/storage/storage.service';
-import { DeleteMediaDTO, GetMediaDTO, MultipleMediaUploadPayload } from './Interfaces/MediaUpload';
+import {
+  DeleteMediaDTO,
+  GetMediaDTO,
+  MultipleMediaUploadPayloadDTO,
+  SuccessfulMediaOperationDTO,
+} from './dto/media.dto';
 
 @ApiTags('Media')
 @Controller('media')
 export class MediaController {
   constructor(private storageService: StorageService) {}
 
-  @Post('')
+  @Post()
+  @ApiCreatedResponse({ type: SuccessfulMediaOperationDTO })
+  @ApiBadRequestResponse({ description: 'Invalid file or file size is too large' })
+  @UseGuards(AdminGuard)
   @UseInterceptors(FilesInterceptor('files'))
   async uploadMedia(
     @UploadedFiles() files: Express.Multer.File[],
-    @Body() mediaUploadPayload: MultipleMediaUploadPayload,
+    @Body() mediaUploadPayload: MultipleMediaUploadPayloadDTO,
   ) {
     const { folder, metadata } = mediaUploadPayload;
     const filesData = files.map((file) => {
@@ -40,6 +50,9 @@ export class MediaController {
   }
 
   @Post('getMedia')
+  @ApiOkResponse({ type: String })
+  @ApiBadRequestResponse({ description: 'Incorrect path' })
+  @UseGuards(AdminGuard)
   async downloadMedia(@Body() dto: GetMediaDTO, @Res() res: Response) {
     // Return the public image URL and its customized metadata
     try {
@@ -47,7 +60,6 @@ export class MediaController {
       const { metadata } = await this.storageService.getMetadata(`media/${dto.path}`);
       res.send({ url, metadata });
     } catch (e) {
-      console.log(e.message);
       if (e.message.toString().includes('No such object')) {
         throw new NotFoundException('image not found');
       } else {
@@ -57,6 +69,10 @@ export class MediaController {
   }
 
   @Post('delete')
+  @ApiOkResponse({ type: SuccessfulMediaOperationDTO })
+  @ApiCreatedResponse({ type: SuccessfulMediaOperationDTO })
+  @ApiBadRequestResponse({ description: 'Invalid path' })
+  @UseGuards(AdminGuard)
   async deleteMedia(@Body() { path }: DeleteMediaDTO, @Res() res: Response) {
     await this.storageService.delete(path);
     return res.status(HttpStatus.OK).json({ success: true });
