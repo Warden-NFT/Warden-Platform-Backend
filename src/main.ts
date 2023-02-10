c1b9a5953d80c376513169c142e85044673c1d1c
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-02-10 14:11:01 +0700

    feat: add bearer authentication to swagger

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setTitle('Warden NFT Platform')
     .setDescription('API documentation for the Warden NFT Platform')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
