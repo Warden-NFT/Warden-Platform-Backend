fd0a4fe6bf5019c71322eda1d012bb65e1f14c48
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-03-19 18:14:32 +0700

    debug: otp on staging

--- src/otp/otp.service.ts
+++ src/otp/otp.service.ts
@@ -5,7 +5,7 @@ import { RequestOtpResponseDTO } from './otp.dto';
 export class OtpService {
   async getOtp(phoneNumber: string): Promise<RequestOtpResponseDTO> {
     try {
-      const sdk = require('api')('@thaibulksms/v1.0#3s3hunt2tktwn9w2l');
+      const sdk = require('api')('@thaibulksms/v1.0#5alni1epl6dge9p1');
       const res = await sdk.postV2OtpRequest(
         {
           key: process.env.OTP_KEY,
@@ -16,13 +16,14 @@ export class OtpService {
       );
       return res;
     } catch (err) {
+      console.log(err);
       throw new HttpException('Failed Requesting OTP', HttpStatus.BAD_REQUEST);
     }
   }
 
   async verifyOtp(token: string, pin: string): Promise<{ status: number; message: string }> {
     try {
-      const sdk = require('api')('@thaibulksms/v1.0#3s3hunt2tktwn9w2l');
+      const sdk = require('api')('@thaibulksms/v1.0#5alni1epl6dge9p1');
       const res = await sdk.postV2OtpVerify(
         {
           key: process.env.OTP_KEY,
