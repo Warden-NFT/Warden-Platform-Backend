bf528cce6e05ecbc2953fad25df77c8d90576af2
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2023-03-22 11:53:48 +0700

    feat: add auth service unit test

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET_KEY'),
+      secretOrKey: configService.get<string>('JWT_SECRET_KEY') ?? 'JWT_SECRET_KEY',
     });
   }
 
